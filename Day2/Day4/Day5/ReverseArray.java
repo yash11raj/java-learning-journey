7436f671aa6d4c9d328b040d5ec76cf8726bc57d
Yash raj <yashxxbgp@gmail.com>
2026-06-25 17:31:44 +0530

    Remaining Day 5 Array programs

---
+++ Day2/Day4/Day5/ReverseArray.java
@@ -0,0 +1,20 @@
+package Day2.Day4.Day5;
+
+import java.util.Scanner;
+
+public class ReverseArray {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+         System.out.println("Enter 5 numbers:");
+        int []num=new int[5];
+        for(int i=0;i<num.length;i++)
+        {
+          num[i]=sc.nextInt();
+        }
+        System.out.println("Reverse of an array is:");
+        for(int i=num.length-1;i>=0;i--)
+        {
+            System.out.println(num[i]);
+    }
+}  
+}
